90da080358398b88ae18c54370dda417db33cc04
SathishSegu <sathishsegu123@gmail.com>
2026-02-25 12:42:49 +0530

    Initial Commit for Project Structure

---
+++ src/main/java/com/flm/mgmtsystem/MultiUserProjectAndTaskMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.flm.mgmtsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiUserProjectAndTaskMgmtSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiUserProjectAndTaskMgmtSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flm/mgmtsystem/MultiUserProjectAndTaskMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flm.mgmtsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiUserProjectAndTaskMgmtSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
